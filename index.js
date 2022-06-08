8e2b320bf9dc7463f22371194617cfdf7fcfa08b
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-08 08:25:35 +0300

    commit tree 65fc4e94680116add1cac2cbfc979e677663dfa9

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+const welcomeMessage = `"Welcome to Flatbook, + ${currentUser}"!`;
+const currentUser2 = "GRACE HOPPER"
+const excitedWelcomeMessage = `"WELCOME TO FLATBOOK, + ${currentUser2}"!`;
+const curentUser1 ="Grace"
+const shortGreeting =`"Welcome, + ${curentUser1}"!`
